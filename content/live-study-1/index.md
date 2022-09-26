cf5c391a5227c2316ad7eaeed1c62a3696e74f93
awesomeo184 <awesomeo184@gmail.com>
2022-09-26 14:32:54 +0900

    test post

---
+++ content/live-study-1/index.md
@@ -0,0 +1,10 @@
+---
+emoji: 📝
+title: [Java Study 1주차] JVM은 무엇이고 자바 코드는 어떻게 실행하는 것인가
+date: '2020-12-28 23:00:00'
+author: 어썸오
+tags: java
+categories: Java-Basic
+---
+
+> 본 포스팅은 백기선님이 진행하시는 [자바 스터디](https://github.com/whiteship/live-study)를 진행하며 혼자 공부하고 이해한 내용을 바탕으로 정리한 글입니다. 오류나 지적 사항이 있다면 댓글로 알려주시면 감사하겠습니다.
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,12 +6,12 @@ module.exports = {
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: `awesomeo184/awesomeo184.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `awesomeo184/awesomeo184.github.io`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `awesomeo184`,
+    name: `정수현`,
     bio: {
       role: `개발자`,
       description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
